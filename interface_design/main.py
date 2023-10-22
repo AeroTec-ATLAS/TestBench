6fa0a5727b459f79b70e64610f8991d2e117ee64
Aramov1 <isacaramov@gmail.com>
2023-10-22 22:15:38 +0100

    Created a dictionary named current_data which will be used to store current values read by sensors. Made updates in the data Container. It is now constinuasly updating with the values in the dictionary. The functions update data is know oloting random values, but it is intended to be be connected to arduino via Serial. Solved fundamental problems regarding the execution of the code, namely, the fact that the programme was not responding due tue misuse of the Qt.Widgets

--- interface_design/main.py
+++ interface_design/main.py
@@ -8,7 +8,7 @@ from PyQt5.QtGui import QPainter
 from Custom_Widgets.Widgets import *
 
 # Import GUI file
-from ui_interface import *
+from ui_interface import Ui_MainWindow
 
 #import math mod
 #from random import randrange
@@ -16,33 +16,69 @@ from functools import partial
 
 # imprt csv
 import csv
+import random
+
+# Dictionary to store sensor data values
+current_data ={
+    "thrust": 0,
+    "torque": 0,
+    "rpm"   : 0,
+    "motor_temp": 0,
+    "esc_temp"  : 0,
+    "voltage"   : 0,
+    "current"   : 0,
+    "airspeed"  : 0,
+    "noise": 0   
+     
+}
 
 
 # Main window class
-class MainWindow(Ui_MainWindow):
-    def __init__(self, parent = None):
-        Ui_MainWindow.__init__(self)
-        self.ui = Ui_MainWindow()
-        self.ui.setupUi(self)
+class MainWindowInterface(Ui_MainWindow):
+    def __init__(self, MainWindow):
+        self.ui = Ui_MainWindow()  # Create an instance of the UI class
+        self.ui.setupUi(MainWindow)      # Set up the UI elements on this QMainWindow instance
 
-        # SET MINIMUM WINDOW SIZE
-        self.setMiniumSize(10000, 2000)
 
-        # Connect button clicks to switching containers in QStackedWidget
-        self.ui.setupBtn.clicked.connect(lambda: self.ui.stackedWidget.setCurrentWidget(self.ui.setupMenu))
-        self.ui.powerTrainBtn.clicked.connect(lambda: self.ui.stackedWidget.setCurrentWidget(self.ui.powerTrainMenu))
-        self.ui.manualControlBtn.clicked.connect(lambda: self.ui.stackedWidget.setCurrentWidget(self.ui.manualControlMenu))
-        self.ui.automaticControlBtn.clicked.connect(lambda: self.ui.stackedWidget.setCurrentWidget(self.ui.automaticControlMenu))
-        self.ui.resultsBtn.clicked.connect(lambda: self.ui.stackedWidget.setCurrentWidget(self.ui.resultsMenu))
+        self.timer = QTimer(MainWindow)       # Create a QTimer instance
+        self.timer.timeout.connect(self.update_data_container)  # Connect timer timeout signal to update function
+        self.timer.start(1000)           # Update every 100 milliseconds (0.1 second)
+    
+    def update_data_container(self):
+        # Update current_data dictionary with random continuous values
+        self.update_data()
+
+        # Update labels with current data values
+        self.ui.ThrustValueText.setText(str(current_data["thrust"]))
+        self.ui.TorqueValueText.setText(str(current_data["torque"]))
+        self.ui.RpmValueText.setText(str(current_data["rpm"]))
+        self.ui.MotorTempValueText.setText(str(current_data["motor_temp"]))
+        self.ui.EscTempValueText.setText(str(current_data["esc_temp"]))
+        self.ui.VoltageValueText.setText(str(current_data["voltage"]))
+        self.ui.CurrentValueText.setText(str(current_data["current"]))
+        self.ui.AirspeedValueText.setText(str(current_data["airspeed"]))
+        self.ui.NoiseValueText.setText(str(current_data["noise"]))
+
+    def update_data(self): # develop serial communication here
+
         
-        #show window
-        self.show()
+        # Assign random continuous values to the current_data dictionary
+        current_data["thrust"] = random.uniform(0, 100)
+        current_data["torque"] = random.uniform(0, 100)
+        current_data["rpm"] = random.uniform(0, 5000)
+        current_data["motor_temp"] = random.uniform(0, 100)
+        current_data["esc_temp"] = random.uniform(0, 100)
+        current_data["voltage"] = random.uniform(0, 50)
+        current_data["current"] = random.uniform(0, 50)
+        current_data["airspeed"] = random.uniform(0, 200)
+        current_data["noise"] = random.uniform(0, 100)
+
 
 #execute app
 if __name__ == '__main__':
-    app = QtWidgets.QApplication(sys.argv)
+    app = QtWidgets.QApplication(sys.argv)  # Create a QApplication instance
     MainWindow = QtWidgets.QMainWindow()
-    ui = Ui_MainWindow()
-    ui.setupUi(MainWindow)
+    ui = MainWindowInterface(MainWindow)         # Create an instance of the MainWindowInterface class
     MainWindow.show()
-    sys.exit(app.exec_())
+    sys.exit(app.exec_())                  # Start the application event loop
+
